02223df10eed12581beb7fa674a98586889d475d
kdao167 <kennydao26@gmail.com>
2024-05-06 17:47:21 -0700

    added additional sub-categories and description

--- DiscordBot/report.py
+++ DiscordBot/report.py
@@ -17,6 +17,9 @@ class State(Enum):
     # Additional states for categories and subcategories
     AWAITING_CATEGORY = auto()
     AWAITING_SUBCATEGORY = auto()
+    AWAITING_SUBSUBCATEGORY = auto()
+    FINISHED_CATEGORY_SELECTIONS = auto()
+    AWAITING_DESCRIPTION = auto()
 
 class CategoryButton(Button):
     def __init__(self, category, report):
@@ -32,7 +35,6 @@ class CategoryButton(Button):
             sub_category_buttons.add_item(SubCategoryButton(sub_cat, self.report))
         await interaction.response.edit_message(content=f"You selected {self.category}. Please select a sub-category:", view=sub_category_buttons)
 
-
 class SubCategoryButton(Button):
     def __init__(self, sub_category, report):
         super().__init__(label=sub_category, style=discord.ButtonStyle.secondary)
@@ -41,14 +43,38 @@ class SubCategoryButton(Button):
 
     async def callback(self, interaction: discord.Interaction):
         self.report.sub_category = self.sub_category
-        self.report.state = State.REPORT_COMPLETE
-        # Clear the buttons once selected and send confirmation message
-        await interaction.response.edit_message(content=f"Sub-category '{self.sub_category}' selected. Thank you for your report. Our moderation team will review this post.", view=None)
+        sub_sub_category_buttons = View()
+        sub_sub_categories = self.report.SUB_SUB_CATEGORIES.get(self.sub_category, [])
+        for sub_sub_cat in sub_sub_categories:
+            sub_sub_category_buttons.add_item(SubSubCategoryButton(sub_sub_cat, self.report))
+        
+        # If there are no additional categories to select. End the report
+        if len(sub_sub_categories) == 0:
+            self.report.state = State.FINISHED_CATEGORY_SELECTIONS
+            await interaction.response.edit_message(content=f"Sub-category '{self.sub_category}' selected.", view=None)
 
-        # Need this fake message to call handle_dm so that it will mark the report as complete and remove it from the reports dictionary
-        # Should mark as pending for moderators when implementing moderator flow
+            # Need this fake message to call handle_dm so that it will mark the report as complete and remove it from the reports dictionary
+            # Should mark as pending for moderators when implementing moderator flow
+            fake_message = type('FakeMessage', (object,), {"author": interaction.user, "content": "end_report", "channel": interaction.channel})
+            await self.report.client.handle_dm(fake_message)
+        else:
+            self.report.state = State.AWAITING_SUBSUBCATEGORY
+            await interaction.response.edit_message(content=f"You selected '{self.sub_category}'. Please select a sub-sub-category:", view=sub_sub_category_buttons)
+
+class SubSubCategoryButton(Button):
+    def __init__(self, sub_sub_category, report):
+        super().__init__(label=sub_sub_category, style=discord.ButtonStyle.secondary)
+        self.sub_sub_category = sub_sub_category
+        self.report = report
+
+    async def callback(self, interaction: discord.Interaction):
+        self.report.sub_sub_category = self.sub_sub_category
+        self.report.state = State.FINISHED_CATEGORY_SELECTIONS
+        await interaction.response.edit_message(content=f"Additional sub-category '{self.sub_sub_category}' selected.", view=None)
+        # Similar to SubCategoryButton, handle ending the report
         fake_message = type('FakeMessage', (object,), {"author": interaction.user, "content": "end_report", "channel": interaction.channel})
         await self.report.client.handle_dm(fake_message)
+
 
 class Report:
     START_KEYWORD = "report"
@@ -56,12 +82,38 @@ class Report:
     HELP_KEYWORD = "help"
 
     # These are placeholders for now until Friday when we finalize our report flow
-    CATEGORIES = ["Harassment", "Spam", "Misinformation"]
     SUB_CATEGORIES = {
-        "Harassment": ["Bullying", "Stalking"],
-        "Spam": ["Bot spam", "Advertisement"],
-        "Misinformation": ["Fake news", "Conspiracy"]
+        "Violence": ["Terrorism", "Threats", "Glorification", "Graphic", "Self-Harm"],
     }
+    SUB_SUB_CATEGORIES = {
+        "Terrorism": ["Recruitment", "Promotion", "Propaganda"]
+    }
+    CATEGORIES = ["Violence", "Sexual", "Copyright", "Harassment", "Misleading", "Inflammatory"]
+
+    SUB_CATEGORIES = {
+        "Violence": ["Terrorism", "Threats", "Glorification", "Graphic", "Self-Harm"],
+        "Sexual": ["Explicit Sexual Activity", "Nudity", "Explicit Text", "Sexual Violence"],
+        "Copyright": ["Plagiarism", "Defamation", "Counterfeit", "Privacy Issues"],
+        "Harassment": ["Bullying", "Child Abuse"],
+        "Misleading": ["Clickbait", "Scams", "Manipulated Media", "Hoax"],
+        "Inflammatory": ["Hate Speech", "Polarizing", "Sensationalism", "Cultural Sensitivity"]
+    }
+
+    SUB_SUB_CATEGORIES = {
+        "Terrorism": ["Recruitment", "Promotion", "Propaganda"],
+        "Self-Harm": ["Suicidal", "Promotion", "Drug Abuse"],
+        "Sexual Violence": [],
+        "Bullying": ["Personal Attacks", "Cyberstalking", "Targeting"],
+        "Child Abuse": ["Grooming", "Physical Abuse", "Emotional Abuse"],
+        "Scams": [],
+        "Manipulated Media": ["Deep Fake", "Edited", "Misattributed", "Out of Context"],
+        "Hate Speech": [],
+        "Polarizing": ["Explicit", "Edited", "Symbols & Gestures"],
+        "Sensationalism": [],
+        "Cultural Sensitivity": ["Appropriation", "Stereotypes", "Symbols & Gestures"],
+    }
+
+
 
     def __init__(self, client):
         self.state = State.REPORT_START
@@ -70,7 +122,9 @@ class Report:
 
         self.category = None
         self.sub_category = None
+        self.sub_sub_category = None
         self.message_author = None
+        self.report_description = None
 
         
     
@@ -120,26 +174,46 @@ class Report:
             self.message_author_name = message.author.name
             return [(("I found this message:\n```" + message.author.name + ": " + message.content + "```" + "Please select the problem:"), category_buttons)]
         
-        if self.state == State.AWAITING_CATEGORY:
-            if message.content in self.CATEGORIES:
-                self.category = message.content
-                self.state = State.AWAITING_SUBCATEGORY
-                return ["Please select a sub-category:", ", ".join(self.SUB_CATEGORIES[self.category])]
-            else:
-                return ["Invalid category. Please try again or say `cancel` to cancel."]
+        if self.state == State.FINISHED_CATEGORY_SELECTIONS:
+            self.state = State.AWAITING_DESCRIPTION
+            return ["Providing further clarification details about the nature of this violation, your report will be prioritized. Suggested topics could include but are not limited to: How did you come across this content? Is this an isolated incident, or part of a larger pattern you’ve observed? What action do you believe should be taken regarding this content?"]
+        
+        if self.state == State.AWAITING_DESCRIPTION:
+            self.report_description = message.content  
+            self.state = State.REPORT_COMPLETE 
+            return [f"Thank you for providing additional details. Your report is now complete and will be reviewed by our moderation team."]
+            
+        
+        # The following states are no longer needed because the category and subcategory are chain selected need for state management.
+                
+        # if self.state == State.AWAITING_CATEGORY:
+        #     if message.content in self.CATEGORIES:
+        #         self.category = message.content
+        #         self.state = State.AWAITING_SUBCATEGORY
+        #         return ["Please select a sub-category:", ", ".join(self.SUB_CATEGORIES[self.category])]
+        #     else:
+        #         return ["Invalid category. Please try again or say `cancel` to cancel."]
 
-        if self.state == State.AWAITING_SUBCATEGORY:
-            if message.content in self.SUB_CATEGORIES[self.category]:
-                self.sub_category = message.content
-                self.state = State.REPORT_COMPLETE
-                return ["Thank you for your report. Our moderation team will review this post. You may choose to mute or block the user."]
-            else:
-                return ["Invalid sub-category. Please try again or say `cancel` to cancel."]
+        # if self.state == State.AWAITING_SUBCATEGORY:
+        #     if message.content in self.SUB_CATEGORIES[self.category]:
+        #         self.sub_category = message.content
+        #         self.state = State.REPORT_COMPLETE
+        #         return ["Thank you for your report. Our moderation team will review this post. You may choose to mute or block the user."]
+        #     else:
+        #         return ["Invalid sub-category. Please try again or say `cancel` to cancel."]
+        
+        # if self.state == State.AWAITING_SUBSUBCATEGORY:
+        #     if message.content in self.SUB_SUB_CATEGORIES[self.sub_category]:
+        #         self.sub_sub_category = message.content
+        #         self.state = State.REPORT_COMPLETE
+        #         return ["Thank you for your report. Our moderation team will review this post. You may choose to mute or block the user."]
+        #     else:
+        #         return ["Invalid sub-sub-category. Please try again or say `cancel` to cancel."]
         
         if self.state == State.REPORT_COMPLETE:
             return []
 
-        return ["An error has occurred. Please restart the reporting process."]
+        return ["An error has occurred. Make sure than any pending reports are completed or canceled. Please restart the reporting process."]
 
     def report_complete(self):
         return self.state == State.REPORT_COMPLETE
