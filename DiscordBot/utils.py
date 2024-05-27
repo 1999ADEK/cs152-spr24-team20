8e1a0c2f688fea7e101e90ea3f76cb2e1ec356b9
1999ADEK <rabbit990326@gmail.com>
2024-05-27 16:31:13 -0700

    Fix node naming conflicts in heap visualization

--- DiscordBot/utils.py
+++ DiscordBot/utils.py
@@ -12,19 +12,19 @@ def visualize_heap(heap, highlight_report=None, highlight_color="grey"):
         right_idx = 2 * idx + 2
 
         if left_idx < len(heap):
-            dot.edge(f"{heap[idx][0]:.2f}", f"{heap[left_idx][0]:.2f}")
+            dot.edge(str(heap[idx]), str(heap[left_idx]))
             add_edges(dot, heap, left_idx)
         
         if right_idx < len(heap):
-            dot.edge(f"{heap[idx][0]:.2f}", f"{heap[right_idx][0]:.2f}")
+            dot.edge(str(heap[idx]), str(heap[right_idx]))
             add_edges(dot, heap, right_idx)
 
     for value in heap:
         score, time, report = value
         if report == highlight_report:
-            dot.node(f"{score:.2f}", shape="circle", style="filled", color=highlight_color)
+            dot.node(str(value), label=f"{score:.2f}", shape="circle", style="filled", color=highlight_color)
         else:
-            dot.node(f"{score:.2f}", shape="circle")
+            dot.node(str(value), label=f"{score:.2f}", shape="circle")
 
     add_edges(dot, heap, 0)
 
